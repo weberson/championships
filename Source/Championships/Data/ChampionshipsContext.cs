c7c5faa0ae0c236ca5f0bab33e4d5c3dbcce429d
Weberson <weberson1990@gmail.com>
2017-01-22 13:54:01 -0200

    Installed EF. Created Player model

---
+++ Source/Championships/Data/ChampionshipsContext.cs
@@ -0,0 +1,19 @@
+﻿using Championships.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Championships.Data
+{
+    public class ChampionshipsContext : DbContext
+    {
+        public ChampionshipsContext() : base()
+        {
+
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ Source/Championships/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace Championships.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+    }
+}
